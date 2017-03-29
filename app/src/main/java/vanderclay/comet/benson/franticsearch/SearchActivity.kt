b2aff0cb965ce377fbcb58864df361389fc3a6bf
Gage Vander Clay <gvanderclay@yahoo.com>
2017-03-29 16:17:30 -0400

    search bar works

--- app/src/main/java/vanderclay/comet/benson/franticsearch/SearchActivity.kt
+++ app/src/main/java/vanderclay/comet/benson/franticsearch/SearchActivity.kt
@@ -1,16 +1,33 @@
 package vanderclay.comet.benson.franticsearch
 
+import android.app.SearchManager
+import android.content.Context
 import android.os.Bundle
+import android.support.v4.view.MenuItemCompat
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.Toolbar
+import android.text.TextUtils
 import android.view.Menu
+import android.view.MenuItem
 import android.widget.ArrayAdapter
 import android.widget.ListView
 import android.widget.SearchView
+import android.widget.SearchView.OnQueryTextListener
 
-class SearchActivity : AppCompatActivity() {
+class SearchActivity : AppCompatActivity(), OnQueryTextListener{
 
-    private var adapter: ArrayAdapter<String>? = null
+    private var _adapter: ArrayAdapter<String>? = null
+    var adapter: ArrayAdapter<String>?
+        get() = _adapter
+        set(value) {
+            _adapter = value
+        }
+    private var _lv: ListView? = null
+    var lv: ListView?
+        get() = _lv
+        set(value) {
+            _lv = value
+        }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,34 +35,40 @@ class SearchActivity : AppCompatActivity() {
         val toolbar = findViewById(R.id.toolbar) as Toolbar
         setSupportActionBar(toolbar)
 
-        val lv = findViewById(R.id.cardSearchList) as ListView
-        var array: Array<String> = arrayOf("This", "Is", "A", "list", "Is", "A", "list", "Is", "A", "list", "Is", "A", "list", "Is", "A", "list", "Is", "A", "list", "Is", "A", "list", "Is", "A", "list", "Is", "A", "list", "Is", "A", "list", "Is", "A", "list", "Is", "A", "list")
+        lv = findViewById(R.id.cardSearchList) as? ListView
+        val array: Array<String> = arrayOf("This", "Is", "A", "list", "Is", "A", "list", "Is", "A", "list", "Is", "A", "list", "Is", "A", "list", "Is", "A", "list", "Is", "A", "list", "Is", "A", "list", "Is", "A", "list", "Is", "A", "list", "Is", "A", "list", "Is", "A", "list")
         adapter = ArrayAdapter(
                 this@SearchActivity,
                 android.R.layout.simple_list_item_1,
                 array)
-        lv.adapter = adapter
+        lv?.adapter = adapter
     }
 
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         val inflater = menuInflater
-        inflater.inflate(R.menu.menu_search, menu)
-        val item = menu?.findItem(R.id.menuSearch)
-        val searchView = item?.actionView as? SearchView
+        inflater.inflate(R.menu.search_menu, menu)
 
-        searchView?.setOnQueryTextListener(object: SearchView.OnQueryTextListener {
-            override fun onQueryTextChange(newText: String?): Boolean {
-                adapter?.filter?.filter(newText)
-                return false
-            }
+        val searchManager = getSystemService(Context.SEARCH_SERVICE) as SearchManager
+        val searchMenuItem = menu?.findItem(R.id.search) as? MenuItem
+        val searchView = MenuItemCompat.getActionView(searchMenuItem) as? SearchView
+        searchView?.setSearchableInfo(searchManager.getSearchableInfo(componentName))
+        searchView?.isSubmitButtonEnabled = true
+        searchView?.setOnQueryTextListener(this)
 
-            override fun onQueryTextSubmit(query: String?): Boolean {
-                return false
-
-            }
-        })
         return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onQueryTextChange(newText: String?): Boolean {
+        if(TextUtils.isEmpty(newText)) {
+
+        }
+        adapter?.filter?.filter(newText)
+        return true
+    }
+
+    override fun onQueryTextSubmit(query: String?): Boolean {
+        return false;
     }
 }
 
