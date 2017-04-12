063f3510388c136906c1c709ca270df35d1e66cb
Gage Vander Clay <gvanderclay@yahoo.com>
2017-04-12 16:45:23 -0400

    finish converting card search activity to fragment

--- app/src/main/java/layout/CardSearchFragment.kt
+++ app/src/main/java/layout/CardSearchFragment.kt
@@ -7,16 +7,14 @@ import android.os.Handler
 import android.support.v4.app.Fragment
 import android.support.v4.view.MenuItemCompat
 import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
 import android.util.Log
-import android.view.LayoutInflater
-import android.view.Menu
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import android.widget.SearchView
 import io.magicthegathering.javasdk.resource.Card
-import kotlinx.android.synthetic.main.fragment_card_search.*
 import org.jetbrains.anko.doAsync
 import org.jetbrains.anko.uiThread
+import vanderclay.comet.benson.franticsearch.MainActivity
 
 import vanderclay.comet.benson.franticsearch.R
 import vanderclay.comet.benson.franticsearch.api.MtgAPI
@@ -32,64 +30,51 @@ import vanderclay.comet.benson.franticsearch.ui.adapters.listeners.EndlessRecycl
  * create an instance of this fragment.
  */
 class CardSearchFragment : Fragment(), SearchView.OnQueryTextListener {
-    private val TAG = "CardSearchActivity"
+    private val TAG = "CardSearchFragment"
 
     private var cardModel = mutableListOf<Card>()
     private var cardAdapter = CardListAdapter(cardModel)
     private var scrollListener: EndlessRecyclerViewScrollListener? = null
     private var cardFilter: String? = ""
     private val handler = Handler()
-
-    // TODO: Rename and change types of parameters
-    private var mParam1: String? = null
-    private var mParam2: String? = null
-
-    private var mListener: OnFragmentInteractionListener? = null
+    private var cardList: RecyclerView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setHasOptionsMenu(true)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?, inflater: MenuInflater?) {
+        super.onCreateOptionsMenu(menu, inflater)
+        menu?.clear()
+        inflater?.inflate(R.menu.search_menu, menu)
+        val item = menu?.findItem(R.id.action_search)
+        val searchView = SearchView((context as MainActivity).supportActionBar?.themedContext)
+        MenuItemCompat.setActionView(item, searchView)
+        searchView.setOnQueryTextListener(this)
+    }
+
+    override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        val rootView = inflater!!.inflate(R.layout.fragment_card_search, container, false)
+        rootView.tag = TAG
+
+        cardList = rootView.findViewById(R.id.cardList) as RecyclerView
+
         val layoutManager = LinearLayoutManager(activity.applicationContext)
-        cardList.layoutManager = layoutManager
+        cardList?.layoutManager = layoutManager
         scrollListener = object: EndlessRecyclerViewScrollListener(layoutManager) {
             override fun onLoadMore(currentPage: Int): Boolean {
                 loadNextDataFromApi(currentPage)
                 return true
             }
         }
-        cardList.addOnScrollListener(scrollListener)
+        cardList?.addOnScrollListener(scrollListener)
         loadNextDataFromApi(1)
-        cardList.setHasFixedSize(true)
-        cardList.adapter = cardAdapter
+        cardList?.setHasFixedSize(true)
+        cardList?.adapter = cardAdapter
+        return rootView
     }
-
-    override fun onPrepareOptionsMenu(menu: Menu?) {
-        super.onPrepareOptionsMenu(menu)
-        val searchMenuItem = menu?.findItem(R.menu.search_menu)
-        val searchView = MenuItemCompat.getActionView(searchMenuItem) as SearchView
-        searchView.setOnQueryTextListener(this)
-    }
-
-    override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
-        return inflater!!.inflate(R.layout.fragment_card_search, container, false)
-    }
-
-    override fun onAttach(context: Context?) {
-        super.onAttach(context)
-        if (context is OnFragmentInteractionListener) {
-            mListener = context as OnFragmentInteractionListener?
-        } else {
-            throw RuntimeException(context!!.toString() + " must implement OnFragmentInteractionListener")
-        }
-    }
-
-    override fun onDetach() {
-        super.onDetach()
-        mListener = null
-    }
-
 
     override fun onQueryTextChange(newText: String?): Boolean {
         cardFilter =  newText
@@ -100,7 +85,7 @@ class CardSearchFragment : Fragment(), SearchView.OnQueryTextListener {
             cardModel.clear()
             scrollListener?.resetState()
 
-            cardList.scrollToPosition(0)
+            cardList?.scrollToPosition(0)
         }, 500)
 
         return true
@@ -121,43 +106,17 @@ class CardSearchFragment : Fragment(), SearchView.OnQueryTextListener {
         return false
     }
 
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     *
-     *
-     * See the Android Training lesson [Communicating with Other Fragments](http://developer.android.com/training/basics/fragments/communicating.html) for more information.
-     */
-    interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        fun onFragmentInteraction(uri: Uri)
-    }
-
     companion object {
-        // TODO: Rename parameter arguments, choose names that match
-        // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-        private val ARG_PARAM1 = "param1"
-        private val ARG_PARAM2 = "param2"
-
         /**
          * Use this factory method to create a new instance of
          * this fragment using the provided parameters.
 
-         * @param param1 Parameter 1.
-         * *
-         * @param param2 Parameter 2.
-         * *
          * @return A new instance of fragment CardSearchFragment.
          */
         // TODO: Rename and change types and number of parameters
-        fun newInstance(param1: String, param2: String): CardSearchFragment {
+        fun newInstance(): CardSearchFragment {
             val fragment = CardSearchFragment()
             val args = Bundle()
-            args.putString(ARG_PARAM1, param1)
-            args.putString(ARG_PARAM2, param2)
-            fragment.arguments = args
             return fragment
         }
     }
--- app/src/main/java/vanderclay/comet/benson/franticsearch/MainActivity.kt
+++ app/src/main/java/vanderclay/comet/benson/franticsearch/MainActivity.kt
@@ -1,62 +1,88 @@
 package vanderclay.comet.benson.franticsearch
 
+import android.content.res.Configuration
 import android.os.Bundle
-import android.support.design.widget.FloatingActionButton
-import android.support.design.widget.Snackbar
-import android.view.View
 import android.support.design.widget.NavigationView
+import android.support.v4.app.Fragment
 import android.support.v4.view.GravityCompat
 import android.support.v4.widget.DrawerLayout
 import android.support.v7.app.ActionBarDrawerToggle
 import android.support.v7.app.AppCompatActivity
-import android.support.v7.widget.Toolbar
 import android.view.Menu
 import android.view.MenuItem
+import android.support.v7.widget.Toolbar
+import kotlinx.android.synthetic.main.toolbar.*
+import layout.CardSearchFragment
 
 class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+    var mDrawer: DrawerLayout? = null
+    var nvDrawer: NavigationView? = null
+    var drawerToggle: ActionBarDrawerToggle? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val toolbar = findViewById(R.id.toolbar) as Toolbar
+
         setSupportActionBar(toolbar)
 
-        val drawer = findViewById(R.id.drawer_layout) as DrawerLayout
-        val toggle = ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
-        drawer.setDrawerListener(toggle)
-        toggle.syncState()
+        mDrawer = findViewById(R.id.drawer_layout) as DrawerLayout?
+        nvDrawer = findViewById(R.id.nvView) as NavigationView?
+        drawerToggle = setUpDrawerToggle()
+        mDrawer?.addDrawerListener(drawerToggle as DrawerLayout.DrawerListener)
 
-        val navigationView = findViewById(R.id.nav_view) as NavigationView
-        navigationView.setNavigationItemSelectedListener(this)
+        setupDrawerContent(nvDrawer)
+
+
     }
 
-    override fun onBackPressed() {
-        val drawer = findViewById(R.id.drawer_layout) as DrawerLayout
-        if (drawer.isDrawerOpen(GravityCompat.START)) {
-            drawer.closeDrawer(GravityCompat.START)
-        } else {
-            super.onBackPressed()
+    private fun setUpDrawerToggle(): ActionBarDrawerToggle {
+        return ActionBarDrawerToggle(this, mDrawer, toolbar, R.string.drawer_open, R.string.drawer_close)
+    }
+
+    private fun setupDrawerContent(navigationView: NavigationView?) {
+        navigationView?.setNavigationItemSelectedListener {
+            selectDrawerItem(it)
+            true
         }
     }
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.main, menu)
-        return true
+    fun selectDrawerItem(menuItem: MenuItem) {
+        val fragment = when(menuItem.itemId) {
+            R.id.card_search -> {
+                CardSearchFragment.newInstance()
+            }
+            else -> CardSearchFragment.newInstance()
+        }
+
+        supportFragmentManager.beginTransaction().replace(R.id.flContent, fragment).commit()
+
+        menuItem.isChecked = true
+
+        title = menuItem.title
+        mDrawer?.closeDrawers()
+    }
+
+    // `onPostCreate` called when activity start-up is complete after `onStart()`
+    // NOTE 1: Make sure to override the method with only a single `Bundle` argument
+    // Note 2: Make sure you implement the correct `onPostCreate(Bundle savedInstanceState)` method.
+    // There are 2 signatures and only `onPostCreate(Bundle state)` shows the hamburger icon.
+    override fun onPostCreate(savedInstanceState: Bundle?) {
+        super.onPostCreate(savedInstanceState)
+        // Sync the toggle state after onRestoreInstanceState has occurred.
+        drawerToggle?.syncState()
+    }
+
+
+    override fun onConfigurationChanged(newConfig: Configuration) {
+        super.onConfigurationChanged(newConfig)
+        // Pass any configuration change to the drawer toggles
+        drawerToggle?.onConfigurationChanged(newConfig)
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        val id = item.itemId
-
-
-        if (id == R.id.action_settings) {
+        if(drawerToggle?.onOptionsItemSelected(item)!!) {
             return true
         }
-
         return super.onOptionsItemSelected(item)
     }
 
@@ -64,17 +90,13 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         // Handle navigation view item clicks here.
         val id = item.itemId
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.card_search) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.decks) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.card_scan) {
 
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.settings_item) {
 
         }
 
