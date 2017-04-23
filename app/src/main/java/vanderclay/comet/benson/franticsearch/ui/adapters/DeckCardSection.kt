5571d1b99ddade8adbc49eaeabe0f3e4fac67437
Gage Vander Clay <gvanderclay@yahoo.com>
2017-04-23 15:15:04 -0400

    add mana symbols to deck view

--- app/src/main/java/vanderclay/comet/benson/franticsearch/ui/adapters/DeckCardSection.kt
+++ app/src/main/java/vanderclay/comet/benson/franticsearch/ui/adapters/DeckCardSection.kt
@@ -6,8 +6,9 @@ import android.widget.ImageView
 import com.squareup.picasso.Picasso
 import io.github.luizgrp.sectionedrecyclerviewadapter.StatelessSection
 import io.magicthegathering.javasdk.resource.Card
+import kotlinx.android.synthetic.main.item_card_row.view.*
 import vanderclay.comet.benson.franticsearch.R
-import vanderclay.comet.benson.franticsearch.model.Deck
+import vanderclay.comet.benson.franticsearch.commons.addManaSymbols
 import vanderclay.comet.benson.franticsearch.ui.adapters.viewholder.CardImageTransform
 import vanderclay.comet.benson.franticsearch.ui.adapters.viewholder.DeckCardHeaderViewHolder
 import vanderclay.comet.benson.franticsearch.ui.adapters.viewholder.DeckCardViewHolder
@@ -23,14 +24,15 @@ class DeckCardSection(val title: String, val cards: Map<Card, Long>?): Stateless
         val deckCardView = holder as DeckCardViewHolder
         val card = cards?.keys?.toList()!![position]
 
-        deckCardView.cardText.text = card.name
-        deckCardView.cardCountText.text = cards[card].toString()
+        deckCardView.cardText.text = "${cards[card]}\t${card.name}"
         Picasso.with(deckCardView.itemView.context)
                 .load(card.imageUrl)
                 .transform(CardImageTransform())
                 .placeholder(R.drawable.no_card)
                 .into(deckCardView.cardImage)
         deckCardView.cardImage.scaleType = ImageView.ScaleType.FIT_XY
+        deckCardView.manaContainer?.removeAllViews()
+        addManaSymbols(card, deckCardView.itemView.context, deckCardView.manaContainer)
     }
 
     override fun getItemViewHolder(view: View?): RecyclerView.ViewHolder {
